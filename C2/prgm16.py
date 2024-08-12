6d7dd5566852dbc47ed363bf2afcc816b3a57817
Keerthana K B <144669335+keerthana-k-b@users.noreply.github.com>
2024-08-12 11:19:26 +0530

    Add files via upload

---
+++ C2/prgm16.py
@@ -0,0 +1,13 @@
+print("SJC23MCA039")
+print("Keerthana K B")
+
+import numpy as np
+A = np.array([[2, 1,-2],[3,0,1],[1,1,-1]])
+b = np.array([-3,5,-2])
+X = np.linalg.solve(A, b)
+print("Matrix A:")
+print(A)
+print("Vector b:")
+print(b)
+print("Solution for X:")
+print(X)
