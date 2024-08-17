1cfa48e880a10d3c79f94cc2c0e3f0d5167a8b59
Keerthana K B <144669335+keerthana-k-b@users.noreply.github.com>
2024-08-17 12:42:25 +0530

    Add files via upload

---
+++ C3/3prgm7.py
@@ -0,0 +1,10 @@
+print("Keerthana K B")
+print("23MCA039")
+
+import pandas as pd
+df = pd.read_csv('iris.csv')
+print("Shape of the dataset is : ", df.shape)
+print("First 5 and last five rows of data set\n", df)
+print("Size of dataset : ",df.size)
+print("No. of samples available for each variety\n", df.count())
+print("Description of the data set\n", df.describe())
